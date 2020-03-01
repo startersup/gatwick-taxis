a97d902a4ba9ad8ed7f66af28d93970868b11bc8
saicharan <saicharan14996@gmail.com>
2020-03-01 11:00:53 +0530

    gatwick
    gatwick

--- quotes/index.php
+++ quotes/index.php
@@ -28,12 +28,14 @@ include($apipath."get_session.php");
     <link rel="stylesheet" href="../css/style.css">
     <script src="../js/jquery.min.js"></script>
     <script src="../js/bootstrap.min.js"></script>
+    <script src="../js/common.js"></script>
+ <script src="../js/cabs.js"></script>
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
     <link href="https://fonts.googleapis.com/css?family=Muli|Niramit|Open+Sans|Roboto" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
-<body>
+<body onload="LoadQuotes();">
 
     <section class="quote">
         <div class="row">
