e887c57da6f50e65c34b898db5ad3d4350cc206f
saicharan <saicharan14996@gmail.com>
2020-03-10 08:23:30 +0530

    seo

--- quotes/index.php
+++ quotes/index.php
@@ -40,7 +40,7 @@ include($apipath."get_session.php");
 
             <div class="col-md-8 nopadding heighto">
                 <div class="quote-card sticky-card">
-                    <p class="dynamic">Taxi Booking from <b id=""><?php echo($_SESSION[$sessionid]['pick']); ?></b> to <b><?php echo($_SESSION[$sessionid]['drop']); ?></b> at 12:00 on 12th september 2018</p>
+                    <p class="dynamic">Taxi Booking from <b id=""><?php echo($_SESSION[$sessionid]['pick']); ?></b> to <b><?php echo($_SESSION[$sessionid]['drop']); ?></b> <?php echo("at ".$_SESSION[$sessionid]['date']." on ".$_SESSION[$sessionid]['hrs'].":".$_SESSION[$sessionid]['min']); ?></p>
                 </div>
                 <div class="row">
                     <div id="mycustomform"> </div>
