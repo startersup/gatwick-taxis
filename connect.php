748d151ce3c15713c712819a6d98effe3db7c1f6
Deepak12051996 <36324331+Deepak12051996@users.noreply.github.com>
2020-06-23 22:35:24 +0530

    db err

--- connect.php
+++ connect.php
@@ -2,4 +2,4 @@
 
 $conn=mysqli_connect('77.104.134.224','britann8_brit','myproject','britann8_brit');
 
-?>`
+?>
--- connection/connect.php
+++ connection/connect.php
@@ -1,6 +1,10 @@
 <?php
 
-$conn=mysqli_connect('sql124.main-hosting.eu','u678426119_mini','myproject','u678426119_mini');
+$conn=mysqli_connect('sql131.main-hosting.eu','u678426119_mini','myproject','u678426119_mini');
+
+
+$conn_seo=mysqli_connect('sql131.main-hosting.eu','u591648374_seo','myproject','u591648374_seo');
+
 
 $conn_site = mysqli_connect('localhost','u351940523_post','DigMyDataFromDB','u351940523_post');
 
