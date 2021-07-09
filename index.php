21af1076f5afd3f1108ce6cb6611e78a20b448e5
saicharan96 <saicharan14996@gmail.com>
2021-07-09 10:12:16 +0530

    push ga code
    push ga code

--- index.php
+++ index.php
@@ -45,6 +45,18 @@ include($spath);
 <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
 <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-02V74FEQ1C"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-02V74FEQ1C');
+</script>
+
+
 </head>
 
 <body>
