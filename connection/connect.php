2123e5aa0f45527f5687b9bbf1cbe7fa6a1db85e
BugHunter1205 <144123064+BugHunter1205@users.noreply.github.com>
2023-09-05 18:16:17 +0530

    Update connect.php
    connection change

--- connection/connect.php
+++ connection/connect.php
@@ -1,11 +1,12 @@
 <?php
 
-$conn=mysqli_connect('sql131.main-hosting.eu','u678426119_mini','Atlas@123','u678426119_mini');
+$conn=mysqli_connect('srv671.hstgr.io','u678426119_mini','Minicabee@123','u678426119_mini');
 
 
 $conn_seo=mysqli_connect('sql131.main-hosting.eu','u591648374_seo','myproject','u591648374_seo');
 
 
-$conn_site = mysqli_connect('localhost','u985267280_gathub','Gatwick@123','u985267280_gathub');
+// $conn_site = mysqli_connect('localhost','u985267280_gathub','Gatwick@123','u985267280_gathub');
+$conn_site=mysqli_connect('srv671.hstgr.io','u678426119_mini','Minicabee@123','u678426119_mini');
 
 ?>
